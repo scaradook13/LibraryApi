49a914046cf02115d473f5db915dd811705c1859
scaradook13 <mythicalwarrior132006@gmail.com>
2025-10-20 12:51:33 +0800

    vercel commit

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-require("./config/database_connections");
 require("dotenv").config();
+require("./config/database_connections");
 const libraryRoutes = require("./routes/libraryRoutes");
 
 app.use(express.json());
 
-
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://your-frontend-domain.vercel.app"],
     credentials: true,
   })
 );
@@ -19,6 +18,4 @@ app.use(morgan("dev"));
 
 app.use("/", libraryRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running at port " + process.env.PORT);
-});
+module.exports = app;
